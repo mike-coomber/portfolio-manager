db0f94ee34a167c06ca3659cd85ead987e0a6987
MC-Coomber <mikecoomber@talktalk.net>
2023-12-17 12:02:59 +0000

    page color picker

--- app/dashboard/editor/components/color-picker.tsx
+++ app/dashboard/editor/components/color-picker.tsx
@@ -1,13 +1,19 @@
-import { Typography, collapse } from "@material-tailwind/react";
+import { Button, Typography, collapse } from "@material-tailwind/react";
 import { useState } from "react";
 import { ChromePicker } from "react-color";
 
 export function ColorPicker({
   onColorSelected,
   initialColor,
+  showInline,
+  onResetPressed,
+  onDeletePressed,
 }: {
   onColorSelected: (color: string) => void;
   initialColor: string | undefined;
+  showInline?: boolean;
+  onResetPressed?: () => void;
+  onDeletePressed?: () => void;
 }) {
   const [open, setOpen] = useState(false);
   const [currentColor, setCurrentColor] = useState(initialColor ?? "white");
@@ -15,12 +21,32 @@ export function ColorPicker({
   return (
     <>
       <div className="flex flex-col relative items-center justify-center">
-        <div
-          className="w-14 h-14 border-black border-solid border"
-          style={{ backgroundColor: initialColor }}
-          onClick={() => setOpen(true)}
-        />
-        <Typography>Background color</Typography>
+        {showInline ? (
+          <div className="flex flex-col justify-center items-center">
+            <Typography>Color picker</Typography>
+            <div className="flex">
+              <Button variant="text" onClick={() => setOpen(true)}>
+                <span className="material-symbols-rounded">colorize</span>
+              </Button>
+              <Button variant="text" onClick={() => onResetPressed!()}>
+                <span className="material-symbols-rounded">restart_alt</span>
+              </Button>
+              <Button variant="text" onClick={() => onDeletePressed!()}>
+                <span className="material-symbols-rounded">delete</span>
+              </Button>
+            </div>
+          </div>
+        ) : (
+          <>
+            <div
+              className="w-14 h-14 border-black border-solid border"
+              style={{ backgroundColor: initialColor }}
+              onClick={() => setOpen(true)}
+            />
+            <Typography>Background color</Typography>
+          </>
+        )}
+
         {open && (
           <ChromePicker
             className="absolute right-0 z-20"
--- app/dashboard/editor/components/page-viewer.tsx
+++ app/dashboard/editor/components/page-viewer.tsx
@@ -4,27 +4,75 @@ import { useState, useContext, useEffect } from "react";
 import { PageIndexContext, ProjectContext } from "../context";
 import { ImagePickerDialog } from "./image-picker-dialog";
 import Image from "next/image";
+import { ColorPicker } from "./color-picker";
+import { ProjectsContext } from "@/app/context/contexts";
 
 export function PageViewer() {
+  const allProjects = useContext(ProjectsContext);
   const { project, setProject } = useContext(ProjectContext);
   const { currentPageIndex } = useContext(PageIndexContext);
 
   const [imagePickerOpen, setImagePickerOpen] = useState(false);
-  const [currentPage, setCurrentPage] = useState(
-    project.pages[currentPageIndex]
-  );
 
-  useEffect(() => {
-    console.log("page viewer", currentPageIndex);
-    setCurrentPage(project.pages[currentPageIndex]);
-  }, [currentPageIndex]);
+  const currentPage = project.pages[currentPageIndex];
+
+  function updatePage(newPage: PageModel) {
+    const newPages = project.pages.filter((val) => val != currentPage);
+
+    newPages.splice(currentPageIndex, 0, newPage);
+    setProject({ ...project, pages: newPages });
+  }
+
+  function changeColor(newColor: string | undefined) {
+    const newPage = {
+      ...currentPage,
+      backgroundColor: newColor,
+    };
+
+    updatePage(newPage);
+  }
+
+  function resetColor() {
+    const originalProject = allProjects.find((val) => val.id == project.id);
+    if (originalProject != undefined) {
+      try {
+        const originalPage = originalProject.pages[currentPageIndex];
+        changeColor(originalPage.backgroundColor);
+      } catch (e) {
+        changeColor(undefined);
+      }
+    }
+  }
 
   return (
-    <div className="flex-1 bg-red-50 p-12">
-      <Typography>Page preview</Typography>
-      <Button variant="text" onClick={() => setImagePickerOpen(true)}>
-        Open Image Picker
-      </Button>
+    <div
+      className="flex-1 p-12"
+      style={{
+        backgroundColor:
+          currentPage.backgroundColor ??
+          project.backgroundColor ??
+          "var(--background)",
+      }}
+    >
+      <div className="flex items-center justify-between">
+        <Typography>Page preview</Typography>
+        <div className="flex">
+          <Button variant="text" onClick={() => setImagePickerOpen(true)}>
+            Open Image Picker
+          </Button>
+          <ColorPicker
+            initialColor={currentPage.backgroundColor}
+            showInline={true}
+            onDeletePressed={() => changeColor(undefined)}
+            onResetPressed={() => {
+              resetColor();
+            }}
+            onColorSelected={(newColor) => {
+              changeColor(newColor);
+            }}
+          />
+        </div>
+      </div>
       <div className="flex">
         {currentPage.images.map((image, index) => (
           <div key={index}>
